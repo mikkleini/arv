101eca98b11733ecb4c7016bf1dba3ac26606e8c
Mikk Leini <mikk.leini@gmail.com>
2023-10-11 23:58:42 +0300

    Kick-off

---
+++ Calc/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace Calc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        /// <summary>
+        /// Window creation override
+        /// </summary>
+        /// <param name="activationState"></param>
+        /// <returns></returns>
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            // Set size
+            window.Width = 600;
+            window.Height = 800;
+
+            // Placement happens after activation
+            window.Activated += Window_Activated;            
+
+            return window;
+        }
+
+        /// <summary>
+        /// Window activation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Window_Activated(object sender, EventArgs e)
+        {
+#if WINDOWS
+            var window = sender as Window;
+
+            // give it some time to complete window resizing task.
+            await window.Dispatcher.DispatchAsync(() => { });
+
+            var disp = DeviceDisplay.Current.MainDisplayInfo;
+
+            // move to screen center
+            window.X = (disp.Width / disp.Density - window.Width) / 2;
+            window.Y = (disp.Height / disp.Density - window.Height) / 2;
+#endif
+        }
+    }
+}
---
+++ Calc/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Maui.Controls;
+using System.Numerics;
+
+namespace Calc
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CreateButtons();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        /// <summary>
+        /// Number buttons column and row offsets
+        /// </summary>
+        private readonly (int col, int row)[] NumberButtonsLayout = new[]
+        {
+            (0, 3), // 0
+            (0, 2), // 1
+            (1, 2), // 2
+            (2, 2), // 3
+            (0, 1), // 4
+            (1, 1), // 5
+            (2, 1), // 6
+            (0, 0), // 7
+            (1, 0), // 8
+            (2, 0), // 9
+        };
+
+        private void CreateButtons()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Button btn = new()
+                {
+                    Text = i.ToString()
+                };
+
+                Grid.SetColumn(btn, NumberButtonsLayout[i].col);
+                Grid.SetRow(btn, 1 + NumberButtonsLayout[i].row);
+
+                ButtonGrid.Add(btn);
+            }
+        }
+    }
+}
